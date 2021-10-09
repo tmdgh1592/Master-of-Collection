81e7debb6b10314d54c34b4ae5a870d7404f221b
tmdgh1592 <tmdgh1592@naver.com>
2021-10-09 21:45:48 +0900

    - GamePickerActivity class is created.

---
+++ app/src/main/java/com/app/buna/boxsimulatorforlol/Activity/GamePickerActivity.kt
@@ -0,0 +1,12 @@
+package com.app.buna.boxsimulatorforlol.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.buna.boxsimulatorforlol.R
+
+class GamePickerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_picker)
+    }
+}
